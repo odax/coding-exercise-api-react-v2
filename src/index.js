9e6c66eb339438f7b38a24f581bdc06bf88af2f5
Nalee Riddell <nriddell@tentcraft.com>
2020-07-14 23:41:42 -0400

    - bootstrap uploader component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { Container, Header } from "semantic-ui-react";
 
 import ResultsList from "./ResultsList";
+import UploadCsv from "./UploadCsv";
 
 const App = ({ children }) => (
   <Container style={{ margin: 20 }}>
@@ -19,6 +20,7 @@ document.head.appendChild(styleLink);
 
 ReactDOM.render(
   <App>
+    <UploadCsv />
     <ResultsList />
   </App>,
   document.getElementById("root")
