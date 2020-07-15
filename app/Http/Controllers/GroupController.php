cd1b7030910f419c1965c5b152af04d59640f51a
Nalee Riddell <nriddell@tentcraft.com>
2020-07-14 22:28:33 -0400

    - group model & controller

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Group;
 
 class GroupController extends Controller
 {
-    //
+    public function index() {
+        return Group::all();
+    }
+    public function show($id) {
+        return Group::find($id);
+    }
+    public function store(Request $request) {
+        return Group::create($request->all());
+    }
+    public function update(Request $request, $id) {
+        $group = Group::findOrFail($id);
+        $group = update($request->all());
+        return $group;
+    }
+    public function delete(Request $request, $id) {
+        $group = Group::findOrFail($id);
+        $group->delete();
+        return 204;
+    }
 }
